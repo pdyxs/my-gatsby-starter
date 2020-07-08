93ff1bb6162b470d4811abaff758fb09224e00da
Paul Sztajer <talk@pdyxs.wtf>
2020-07-08 15:49:56 +1000

    adding markdown, material-ui, react-helmet, emotion

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import styled from "@emotion/styled"
+
+const Content = styled.div`
+  text-align: center;
+`;
 
 const Title: React.FC<{}> = () => {
-  return <h1>Hello TypeScript!</h1>;
+  return (
+    <Content>
+      <h1>Hello TypeScript!</h1>
+      <Button variant="contained">Test Button</Button>
+    </Content>
+  );
 };
 
 export default Title;
